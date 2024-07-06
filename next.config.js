bf8089fb5e9925b63dc07140bd45535230529d20
Said Kadirov <ra9@MacBook-Pro-Said.local>
2024-07-07 01:09:14 +0300

    ...

--- next.config.js
+++ next.config.js
@@ -3,11 +3,4 @@ const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
     basePath: isProd ? '/innopolis' : '',
     assetPrefix: isProd ? '/innopolis/' : '',
-    output: 'standalone',
-    exportPathMap: function (defaultPathMap) {
-        return {
-            '/': { page: '/' },
-            '/comic': { page: '/comic' },
-        };
-    },
 };
