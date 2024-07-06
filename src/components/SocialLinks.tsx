05a1fc27f1a9eed97ee5cdc665850ca0475966bd
Said Kadirov <ra9@MacBook-Pro-Said.local>
2024-07-07 00:35:00 +0300

    final vers

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 
+const basePath = process.env.NODE_ENV === 'production' ? '/innopolis' : '';
+
 const SocialLinks: React.FC = () => (
     <div className="social-links">
         <a id="gh-link" href="https://github.com/inopolis" target="_blank" rel="noopener noreferrer">
-            <img src="/images/github-mark.png" alt="GitHub" />
+            <img src={`${basePath}/images/github-mark.png`} alt="GitHub" />
         </a>
         <a id="tg-link" href="https://t.me/Saidaziz_K" target="_blank" rel="noopener noreferrer">
-            <img src="/images/telegram-icon.png" alt="Telegram" />
+            <img src={`${basePath}/images/telegram-icon.png`} alt="Telegram" />
         </a>
         <a id="email" href="mailto:s.kadirov@innopolis.university">
-            <img src="/images/email-icon.png" alt="Email" />
+            <img src={`${basePath}/images/email-icon.png`} alt="Email" />
         </a>
     </div>
 );
+
 
 export default SocialLinks;
