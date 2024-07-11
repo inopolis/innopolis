5ea540668a9bea6561bfb2628f8d4569f5518571
Said Kadirov <ra9@MacBook-Pro-Said.local>
2024-07-11 22:52:26 +0300

    hw-6 final deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
-const isProd = process.env.NODE_ENV === 'production';
-
 module.exports = {
-    basePath: isProd ? '/innopolis' : '',
-    assetPrefix: isProd ? '/innopolis/' : '',
-    images: {
-        domains: ['imgs.xkcd.com'],
-        unoptimized: true, // Отключаем оптимизацию изображений
-    },
+  images: {
+    domains: ['imgs.xkcd.com'],
+    unoptimized: true,
+  },
 };
--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -1,7 +1,6 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
 import Image from 'next/image';
-
-const basePath = process.env.NODE_ENV === 'production' ? '/innopolis' : '';
 
 const SocialLinks: React.FC = () => (
   <div className="social-links">
@@ -12,7 +11,7 @@ const SocialLinks: React.FC = () => (
       rel="noopener noreferrer"
     >
       <Image
-        src={`${basePath}/images/github-mark.png`}
+        src="/images/github-mark.png"
         alt="GitHub"
         width={32}
         height={32}
@@ -25,7 +24,7 @@ const SocialLinks: React.FC = () => (
       rel="noopener noreferrer"
     >
       <Image
-        src={`${basePath}/images/telegram-icon.png`}
+        src="/images/telegram-icon.png"
         alt="Telegram"
         width={32}
         height={32}
@@ -33,7 +32,7 @@ const SocialLinks: React.FC = () => (
     </a>
     <a id="email" href="mailto:s.kadirov@innopolis.university">
       <Image
-        src={`${basePath}/images/email-icon.png`}
+        src="/images/email-icon.png"
         alt="Email"
         width={32}
         height={32}
