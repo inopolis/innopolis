3c74760e1f878bb4bcebd9b6272c15caf2d28c88
Said Kadirov <ra9@MacBook-Pro-Said.local>
2024-07-11 21:18:56 +0300

    hw-6

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,19 @@
 import '../styles/globals.css';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function Innopolis({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <title>Innopolis Portfolio</title>
+                <meta name="description" content="Portfolio of Saidaziz, a developer passionate about technology." />
+                <meta name="author" content="Saidaziz Kadirov" />
+                <link rel="icon" href="/images/favicon.ico" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
-export default Innopolis
+export default Innopolis;
