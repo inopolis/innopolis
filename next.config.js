63c07f0278a3adf57eed2cb6475587cef2f6dc05
Said Kadirov <ra9@MacBook-Pro-Said.local>
2024-07-11 21:51:26 +0300

    hw-6

--- next.config.js
+++ next.config.js
@@ -3,4 +3,8 @@ const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
     basePath: isProd ? '/innopolis' : '',
     assetPrefix: isProd ? '/innopolis/' : '',
+    images: {
+        domains: ['imgs.xkcd.com'], // Добавьте сюда нужные домены
+    },
+    output: 'export',
 };
